00f7a909b3adf5f495b4c3de52e3e745be7c7218
7raul7k <balanescuraul@gmail.com>
2023-09-20 21:01:20 +0300

    add home-component

---
+++ src/app/service/student.service.ts
@@ -0,0 +1,15 @@
+import {Injectable, OnDestroy, OnInit} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService implements OnInit,OnDestroy{
+
+  constructor() { }
+
+  ngOnDestroy() {
+  }
+
+  ngOnInit() {
+  }
+}
